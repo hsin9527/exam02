bffe1d4b0a57a21014d157967f1dfbe6889d85d4
ee2405 <dean03318Wgmail.com>
2020-04-29 16:35:07 +0800

    1

---
+++ 1/fft.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import serial
+import time
+
+
+Fs = 300
+
+t = np.arange(0,10,1/10)
+
+x = np.zeros(100) 
+y = np.zeros(100)
+z = np.zeros(100) 
+
+serdev = '/dev/ttyACM0'
+
+s = serial.Serial(serdev,115200)
+
+for i in range(300):
+    line=s.readline()
+    #print(line)
+    if(i%3 == 0): x[int(i/3)] = float(line)
+    elif(i%3 == 1): y[int(i/3)] = float(line)
+    elif(i%3 == 2): z[int(i/3)] = float(line)
+
+disa = np.zeros(100)
+disb = np.zeros(100)
+dis = np.zeros(100)
+great =  np.zeros(100)
+for i in range(100):
+    tu = 0.05
+    if (i==0):
+        disa[i]=0
+        disb[i]=0
+    else:
+        disa[i]=disa[i-1]+x[i-1]*9.8/2*0.1*0.1
+        disb[i]=disb[i-1]+y[i-1]*9.8/2*0.1*0.1
+    dis[i] = np.sqrt(disa[i]*disa[i]+disb[i]*disb[i])
+    if dis[i] > tu:   great[i] = 1
+    else:   great[i] = 0
+
+fig, ax = plt.subplots(2, 1)
+
+ax[0].plot(t,x, label = 'x')
+ax[0].plot(t,y, label = 'y')
+ax[0].plot(t,z, label = 'z')
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Acc Vector')
+ax[0].legend()
+
+ax[1].stem(t,great) 
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('displacement larger than 5')
+
+plt.show()
+
+s.close()
---
+++ 1/main.cpp
@@ -0,0 +1,102 @@
+
+#include "mbed.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#define UINT14_MAX        16383
+// FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+// FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+I2C i2c( PTD9,PTD8);
+Serial pc(USBTX, USBRX);
+InterruptIn button(SW3);
+DigitalOut led(LED1);
+int  m_addr= FXOS8700CQ_SLAVE_ADDR1;
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
+void FXOS8700CQ_writeRegs(uint8_t * data, int len);
+
+EventQueue queue;
+Thread thr;
+Timer timer1;
+float temp[3];
+
+void tilting(){
+   timer1.reset();
+   timer1.start();
+   while(timer1.read()<10.1){
+      pc.printf("%1.4f\r\n",temp[0]);
+      pc.printf("%1.4f\r\n",temp[1]);
+      pc.printf("%1.4f\r\n",temp[2]);
+      wait(0.1);
+      led = !led;
+   }
+}
+
+void Trig_led1()  {
+      queue.call(tilting);
+   
+}
+
+int main() {
+   
+   pc.baud(115200);
+   
+   uint8_t who_am_i, data[2], res[6];
+   int16_t acc16;
+
+   FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+   data[1] |= 0x01;
+   data[0] = FXOS8700Q_CTRL_REG1;
+   FXOS8700CQ_writeRegs(data, 2);
+   // Get the slave address
+
+   FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
+
+   timer1.start();
+   led = 1;
+   thr.start(callback(&queue, &EventQueue::dispatch_forever));
+   button.rise(Trig_led1);
+
+   while (true) {
+      FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+      acc16 = (res[0] << 6) | (res[1] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      temp[0] = ((float)acc16) / 4096.0f;
+      acc16 = (res[2] << 6) | (res[3] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      temp[1] = ((float)acc16) / 4096.0f;
+      acc16 = (res[4] << 6) | (res[5] >> 2);
+      if (acc16 > UINT14_MAX/2)
+         acc16 -= UINT14_MAX;
+      temp[2] = ((float)acc16) / 4096.0f;
+   }
+}
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+
+   char ta = addr;
+   i2c.write(m_addr, &ta, 1, true);
+   i2c.read(m_addr, (char *)data, len);
+
+}
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+   i2c.write(m_addr, (char *)data, len);
+
+}
